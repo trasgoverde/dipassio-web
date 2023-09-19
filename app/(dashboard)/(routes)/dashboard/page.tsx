1c15c0e51a1619d216fbbc40629e12fec2596dff
Ignacio <ign.souto@gmail.com>
2023-09-19 13:06:35 +0200

    Sidebar in Dashboard :rocket: :rocket:

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
-import Link from "next/link";
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page</p>
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
