3d8727e9e2223ae0c2911f03afd5fc27b6f3c3c8
Ignacio <ign.souto@gmail.com>
2023-09-20 15:22:03 +0200

    sidebar update, logo and brand :fire: :fire:

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import  Navbar  from "@/components/navbar";
+import Sidebar from "@/components/sidebar"
 
 const DashboardLayout = ({
     children,
@@ -10,10 +11,7 @@ const DashboardLayout = ({
       <div className="h-full relative">
         <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed 
         md:inset-y-0 z-80 bg-blue-900">
-          
-          <div>
-            Hello Sidebar
-           </div> 
+        <Sidebar />
         </div>
         <main className="md:pl-72">
           <Navbar />
---
+++ components/sidebar.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({
+    weight: "600",
+     subsets: ["latin"] 
+});
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full bg-[#103fef] text-white">
+            <div className="px-3 py-2 flex-1">
+                <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+                    <div className="relative w-8 h-8 mr-4">
+                        <Image
+                        fill
+                        alt="Logo"
+                        src="/rhinologo best.svg"
+                        />
+                    </div>
+                    <h1 className={cn("text-2xl font-bold", montserrat.className)}>DIPASSIO</h1>
+                  
+                </Link>
+                    {/* <h2 className={cn("text-sm font-light", montserrat.className)}>DIPASSIO</h2> */}
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
