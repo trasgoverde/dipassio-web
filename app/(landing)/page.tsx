fdd66453c773c60e187c6686b2a4c79494d9ffe9
Ignacio <ign.souto@gmail.com>
2023-10-04 17:35:09 +0200

    Feature section in Landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,21 +2,13 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image"
 
 
 const LandingPage = () => {
   return (
    
-    // <div>
-    //   Landing Page (unprotected)
-    //   <Link href="/sign-in">
-    //     <Button>Login</Button>
-    //   </Link>
-    //   <Link href="/sign-up">
-    //     <Button >Register</Button>
-    //   </Link>
-    //   <div> </div>
-    // </div>
+    <>
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transistion-all hover:border-gray-300 hover:bg-white/50">
         <p> 
@@ -24,10 +16,10 @@ const LandingPage = () => {
         </p>
       </div>
       <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
-        Simple, <span className='text-blue-600'>Powerful  </span> Boost for your Projects
+      Professional Solutions <span className='text-blue-600'> & Powerful Applications </span> Tailored to your Business needs
         </h1>
         <p className='mt-5 max-wprose text-zinc-700 sm:text-lg'>
-          Dipassio is a digital passport through innovation. It allows you to boost your projects by connecting them with the best talents around the world
+          Dipassio is a digital product agency that focuses on strategy and design.
           </p>
           <Link 
           className={buttonVariants({
@@ -39,8 +31,71 @@ const LandingPage = () => {
           Get Started! <ArrowRight className='ml-2 h-5 w-5' />
           </Link>
       </MaxWidthWrapper>
+       
+       {/* value proposition section */}
+       <div>
+       <div className='relative  isolate'>
+        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+          <div style={{
+            clipPath: "`polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)`"
+          }} className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'/> 
 
+          </div>
+
+          <div>
+            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image 
+                  src='/empty.png'
+                  alt="empty"
+                  width={1364}
+                  height={866}
+                  quality={100}
+                  className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+            </div>
+          </div>
+        </div>
+        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+          <div style={{
+            clipPath: "`polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)`"
+          }} className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'/> 
+
+          </div>
+
+       </div>
+       </div>
+
+       {/* Feature Section */ }
+       <div className='mx-auto mb-32 mt-32 max-w5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Start your Journey with us in minutes
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+            Developing your Projects has never been easier than with Dipassio Agency.
+            </p>
+          </div>
+        </div>  
+      </div>      
+     </>
   );
 };
 
 export default LandingPage;
+
+
+    {/* / <div>
+    //   Landing Page (unprotected)
+    //   <Link href="/sign-in">
+    //     <Button>Login</Button>
+    //   </Link>
+    //   <Link href="/sign-up">
+    //     <Button >Register</Button>
+    //   </Link>
+    //   <div> </div>
+    // </div>
+*/}
