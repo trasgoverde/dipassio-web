3323fc18d52ccddd07ece55278c384668dd4a926
Ignacio <ign.souto@gmail.com>
2023-10-05 13:59:49 +0200

    Some texts in dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,13 +15,16 @@ export default function HomePage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the Power of Your AI Assistant
+          Project Dashboard
+        </h2>
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Let&apos;s Get Started!!
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+         Enjoy the Experience - Just Upload your Assets, Logos and Info regarding your Project/s
         </p>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          PdfChat, Email Genereration, Blog Content, SEO, Linkedin Posts - All in one
+        Collaborate on shared projects, track progress, and manage budgeting from one dashboard. - All in one
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
